bc737d595fb43d4f6ac1c5b3f4dfaf00cb2ee378
Sepandard <Sepandard@systemgroup.net>
2022-12-21 12:45:18 +0330

    feat (api) add swagger to admin api

--- apps/admin.api/src/main.ts
+++ apps/admin.api/src/main.ts
@@ -1,22 +1,21 @@
-/**
- * This is not a production server yet!
- * This is only a minimal backend to get started.
- */
+// src/main.ts
 
-import { Logger } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
-
 import { AppModule } from './app/app.module';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  const globalPrefix = 'api';
-  app.setGlobalPrefix(globalPrefix);
-  const port = process.env.PORT || 3333;
-  await app.listen(port);
-  Logger.log(
-    `🚀 Application is running on: http://localhost:${port}/${globalPrefix}`
-  );
-}
 
+  const config = new DocumentBuilder()
+    .setTitle('HitFlow')
+    .setDescription('The Hit Flow API description')
+    .setVersion('0.1')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
+  await app.listen(3000);
+}
 bootstrap();
