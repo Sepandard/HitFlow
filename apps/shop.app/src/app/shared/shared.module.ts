dd1bd7446c71592a7a974ac00ecc13882845a7ce
Sepandard <Sepandard@systemgroup.net>
2023-01-28 00:52:01 +0330

    feat (shared) apply footer #35

--- apps/shop.app/src/app/shared/shared.module.ts
+++ apps/shop.app/src/app/shared/shared.module.ts
@@ -5,16 +5,17 @@ import { MatInputModule } from '@angular/material/input';
 import { MatIconModule } from '@angular/material/icon';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatButtonModule } from '@angular/material/button';
+import { FooterComponent } from './components/footer/footer.component';
 
-const MAT_MODULES : Provider[] = [
+const MAT_MODULES: Provider[] = [
   MatIconModule,
   MatFormFieldModule,
   MatInputModule,
   MatButtonModule,
-]
+];
 @NgModule({
-  declarations: [COMPONENT],
-  imports: [MAT_MODULES,CommonModule],
-  exports:[COMPONENT,MAT_MODULES]
+  declarations: [COMPONENT, FooterComponent],
+  imports: [MAT_MODULES, CommonModule],
+  exports: [COMPONENT, MAT_MODULES],
 })
 export class SharedModule {}
