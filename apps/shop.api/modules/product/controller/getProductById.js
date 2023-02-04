bcbbd9b016a5872f8d2d2bf76543843f57754736
Sepandard <Sepandard@systemgroup.net>
2023-02-04 13:08:05 +0330

    feat (product) add product get by id #41

--- apps/shop.api/modules/product/controller/getProductById.js
+++ apps/shop.api/modules/product/controller/getProductById.js
@@ -1,13 +1,19 @@
 const asyncHandler = require('../../../middlewares/async');
 const client = require('../../../config/db.js');
-const ResponseMessages = require('../../../contract/responseMessages')
+const ResponseMessages = require('../../../contract/responseMessages');
 
 exports.getById = asyncHandler(async (req, res, next) => {
-    if (!Number(req.params.id)) {
-        return next(new ErrorHandler(res, ResponseMessages.INVALID_ID, 400));
-      }
-    await client.query(
-      `
+  if (!Number(req.params.id)) {
+    return next(
+      new ErrorHandler(
+        res,
+        ResponseMessages.INVALID_ID,
+        400
+      )
+    );
+  }
+  await client.query(
+    `
       SELECT 
       prod."id"
       ,prod."categoryId"
@@ -15,25 +21,32 @@ exports.getById = asyncHandler(async (req, res, next) => {
       ,prod.image
       ,prod.description
       ,prod.amount
+      ,prod.cost  
 	  ,cat.title as categoryTitle
         from public.product prod
 	    INNER JOIN public.category cat  on prod."categoryId" = cat."id"
-        WHERE "isDeleted" = 0 AND id = $1
+	    INNER JOIN public.comment com  on prod."categoryId" = cat."id"
+        WHERE "isDeleted" = 0 AND prod."id" = $1
         ORDER BY id ASC
     `,
-      [Number(req.params.id)],
-      (err, result) => {
-        if (!err) {
-          if (result) {
-            res.status(200).json(result.rows);
-          } else {
-            res.status(500).json('something went wrong');
-          }
+    [Number(req.params.id)],
+    (err, result) => {
+      if (!err) {
+        if (result) {
+          const data = result.rows.map((item) => {
+            return {
+              ...item,
+              image: `http://localhost:5020/${item.image}`
+            };
+          });
+          res.status(200).json(data[0]);
         } else {
-          console.log(err);
-          res.status(500).json(err);
+          res.status(500).json('something went wrong');
         }
+      } else {
+        console.log(err);
+        res.status(500).json(err);
       }
-    );
-  });
-  
+    }
+  );
+});
