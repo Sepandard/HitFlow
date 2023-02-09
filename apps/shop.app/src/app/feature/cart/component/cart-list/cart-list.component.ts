404e3169acc020f104ab0ca56e132a8ebf981d52
kiarashhamdi <kiarashhamdi@gmail.com>
2023-02-09 18:59:53 +0330

    feat (cart) add cart list

--- apps/shop.app/src/app/feature/cart/component/cart-list/cart-list.component.ts
+++ apps/shop.app/src/app/feature/cart/component/cart-list/cart-list.component.ts
@@ -5,4 +5,30 @@ import { Component } from '@angular/core';
   templateUrl: './cart-list.component.html',
   styleUrls: ['./cart-list.component.scss']
 })
-export class CartListComponent {}
+export class CartListComponent {
+  finalPrice = "30000000";
+  data = [
+    {
+      productName: "کتاب",
+      productCost: "10000000",
+      catagory: "کتاب",
+      imgSrc: "../../../../../assets/photo/about/bookImage.jpg"
+    },
+    {
+      productName: "کتاب",
+      productCost: "10000000",
+      catagory: "کتاب",
+      imgSrc: "../../../../../assets/photo/mock/2.jfif"
+    },
+    {
+      productName: "کتاب",
+      productCost: "10000000",
+      catagory: "کتاب",
+      imgSrc: "../../../../../assets/photo/mock/1.jfif"
+    },
+  ];
+  buyProducts(){
+    console.log("Buy");
+    
+  }
+}
