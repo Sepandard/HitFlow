342323d4a77ec9c5934c45bbab356c2b1c6f5d9c
kiarashhamdi <kiarashhamdi@gmail.com>
2023-02-09 19:02:32 +0330

    feat (cart) import delivered items component

--- apps/shop.app/src/app/feature/cart/cart.module.ts
+++ apps/shop.app/src/app/feature/cart/cart.module.ts
@@ -3,9 +3,14 @@ import { CommonModule } from '@angular/common';
 
 import { CartRoutingModule } from './cart-routing.module';
 import { CartListComponent } from './component/cart-list/cart-list.component';
+import { SharedModule } from '@shared/shared.module';
+import { DeliveredItemsComponent } from './component/delivered-items/delivered-items.component';
 
 @NgModule({
-  declarations: [CartListComponent],
-  imports: [CommonModule, CartRoutingModule]
+  declarations: [
+    CartListComponent,
+    DeliveredItemsComponent
+  ],
+  imports: [CommonModule, CartRoutingModule, SharedModule]
 })
 export class CartModule {}
