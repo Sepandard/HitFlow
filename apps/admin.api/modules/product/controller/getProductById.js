2140ac38188842897a7287114894144d5c8486e1
Sepandard <Sepandard@systemgroup.net>
2023-02-11 20:35:41 +0330

    feat (admin) add get product by id  #50

--- apps/admin.api/modules/product/controller/getProductById.js
+++ apps/admin.api/modules/product/controller/getProductById.js
@@ -3,7 +3,7 @@ const ResponseMessages = require('../../../contract/responseMessages');
 const ErrorHandler = require('../../../utils/errorHandler');
 
 
-exports.getAll = asyncHandler(async (req, res, next) => {
+exports.getById = asyncHandler(async (req, res, next) => {
     if (!Number(req.params.id)) {
         return next(new ErrorHandler(res, ResponseMessages.INVALID_ID, 400));
       }
@@ -19,9 +19,9 @@ exports.getAll = asyncHandler(async (req, res, next) => {
 	  ,cat.title as categoryTitle
         from public.product prod
 	    INNER JOIN public.category cat  on prod."categoryId" = cat."id"
-        WHERE "isDeleted" = 0
+        WHERE "isDeleted" = 0 AND id = $1
         ORDER BY id ASC
-        WHERE id = $1
+      
     `,
       [Number(req.params.id)],
       (err, result) => {
