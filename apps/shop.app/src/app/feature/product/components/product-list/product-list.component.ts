e39056689559f800a355ee5e4a5df0e565b4f282
Sepandard <Sepandard@systemgroup.net>
2023-02-14 23:17:14 +0330

    feat (shop) apply search in product #50

--- apps/shop.app/src/app/feature/product/components/product-list/product-list.component.ts
+++ apps/shop.app/src/app/feature/product/components/product-list/product-list.component.ts
@@ -3,7 +3,8 @@ import {
   Component,
   Input
 } from '@angular/core';
-import { finalize } from 'rxjs';
+import { ActivatedRoute } from '@angular/router';
+import { finalize, switchMap } from 'rxjs';
 import { Product } from '../../api/product-api.model';
 import { ProductApiService } from '../../api/product-api.service';
 
@@ -16,7 +17,10 @@ export class ProductListComponent implements AfterViewInit {
   @Input() hasOff?: boolean = false;
   public loading: boolean = false;
   public data: Product[] = [];
-  constructor(private api: ProductApiService) {}
+  constructor(
+    private api: ProductApiService,
+    private route: ActivatedRoute
+  ) {}
   ngAfterViewInit() {
     if (!this.hasOff) {
       this.getData();
@@ -26,10 +30,15 @@ export class ProductListComponent implements AfterViewInit {
   }
 
   getData() {
-    this.loading = true;
-    this.api
-      .getAll()
-      .pipe(finalize(() => (this.loading = false)))
+    this.route.queryParams
+      .pipe(
+        switchMap((searchParams) => {
+          this.loading = true;
+          return this.api
+            .getAll(searchParams)
+            .pipe(finalize(() => (this.loading = false)));
+        })
+      )
       .subscribe({
         next: (result) => {
           this.data = result;
