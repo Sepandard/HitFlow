7e7264c685f963c611a8d8b7cf04a883378f5e05
Sepandard <Sepandard@systemgroup.net>
2023-05-18 19:04:05 +0330

    feat(core) add suggest component

---
+++ apps/shop/src/app/core/components/suggest/suggest.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'hf-suggest',
+  templateUrl: './suggest.component.html',
+  styleUrls: ['./suggest.component.scss'],
+})
+export class SuggestComponent {}
