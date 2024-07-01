e221b3a8d326ef387d2350c334a114ae1dc8637e
s.derakhshande <s.derakhshande@mydigipay.com>
2024-07-01 15:48:48 +0330

    feat(core) configure feedback form

--- apps/shop/src/app/core/core.module.ts
+++ apps/shop/src/app/core/core.module.ts
@@ -14,6 +14,7 @@ import { MatIconModule } from '@angular/material/icon';
 import { MatButtonModule } from '@angular/material/button';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -33,6 +34,7 @@ import { MatInputModule } from '@angular/material/input';
     MatButtonModule,
     MatFormFieldModule,
     MatInputModule,
+    FormsModule
   ],
   exports: [LayoutComponent],
   providers: [
