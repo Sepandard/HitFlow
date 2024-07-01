7bbd6a65b41500dec07b03c5ce91c9f010f33547
s.derakhshande <s.derakhshande@mydigipay.com>
2024-07-01 15:48:02 +0330

    feat(core) Implement feedback component

--- apps/shop/src/app/core/components/feedback/feedback.component.ts
+++ apps/shop/src/app/core/components/feedback/feedback.component.ts
@@ -1,40 +1,48 @@
 import { Component, inject, QueryList, ViewChildren } from '@angular/core';
 import { MatBottomSheetRef } from '@angular/material/bottom-sheet';
+import { FeedbackApiService } from '@app/core/api/feedback-api.service';
+import { FeedbackFeeling } from '@app/core/api/feedback.model';
+import { NotificationsService } from '../../services/notifications.service';
 
 @Component({
   selector: 'hf-feedback',
   templateUrl: './feedback.component.html',
   styleUrls: ['./feedback.component.scss'],
+  providers: [FeedbackApiService],
 })
 export class FeedbackComponent {
   @ViewChildren('feedbacks') feedbacks: QueryList<HTMLElement>;
   selectedElement: any = {};
+  message: string = '';
   feedbackItems = [
     {
       label: '1',
-      value: 1,
+      value: FeedbackFeeling.TERRIBLE,
     },
     {
       label: '2',
-      value: 2,
+      value: FeedbackFeeling.BAD,
     },
     {
       label: '3',
-      value: 3,
+      value: FeedbackFeeling.NORMAL,
     },
     {
       label: '4',
-      value: 4,
+      value: FeedbackFeeling.GOOD,
     },
     {
       label: '5',
-      value: 5,
+      value: FeedbackFeeling.FANATIC,
     },
   ];
 
   private _bottomSheetRef: MatBottomSheetRef<FeedbackComponent> = inject(
     MatBottomSheetRef<FeedbackComponent>
   );
+
+  private api = inject(FeedbackApiService);
+  private notification = inject(NotificationsService);
 
   onActiveFeedback(item) {
     if (item.value !== this.selectedElement.value)
@@ -52,6 +60,15 @@ export class FeedbackComponent {
   }
 
   submitForm() {
-    this._bottomSheetRef.dismiss(this.selectedElement);
+    this.api.postFeedback({
+      feeling: this.selectedElement.value,
+      message: this.message
+    }).subscribe({
+      next:(data)=>{
+        this._bottomSheetRef.dismiss(data[0]);
+        this.notification.showSuccess('Thanks for your feedback')
+        this.message = ''
+      }
+    })
   }
 }
