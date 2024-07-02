378335e7807aed5f4553b60a2f0255a1d2dc0f12
s.derakhshande <s.derakhshande@mydigipay.com>
2024-07-02 21:21:38 +0330

    refactor(shop) root route in project

--- apps/shop/src/app/app.routes.ts
+++ apps/shop/src/app/app.routes.ts
@@ -2,21 +2,26 @@ import { Route } from '@angular/router';
 import { LayoutComponent } from '@app/core/components';
 
 export const appRoutes: Route[] = [
-
+  {
+    path: 'auth',
+    loadChildren: () => import('./auth/auth.module').then((m) => m.AuthModule),
+  },
   {
     path: '',
     component: LayoutComponent,
     children: [
+ 
       {
-        path: 'auth',
-        loadChildren: () => import('./auth/auth.module').then((m) => m.AuthModule),
-      },
-      {
-        path: 's',
+        path: 'shop',
         loadComponent: () =>
           import('./features/home/home.component').then((m) => m.HomeComponent),
       },
+      {
+        path:'**',
+        redirectTo: '/shop'
+      }
     ],
+    
     
   },
 ];
